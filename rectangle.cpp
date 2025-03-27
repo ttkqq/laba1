f3851a45b4eafe4cf86952c013a27d2256d23ea1
lemonchiklee <Krasnoshtanovasonya@gmail.com>
2025-03-27 07:19:34 +0000

    added comment

--- rectangle.cpp
+++ rectangle.cpp
@@ -37,7 +37,7 @@ float diagonal(ushort_int a, ushort_int b) {
 
 int main() {
     setlocale(LC_ALL, "rus");
-    cout << "это программа для счёта периметра, площади и длины диагонали прямоугольника" << endl;
+    cout << "Eто программа для счёта периметра, площади и длины диагонали прямоугольника" << endl;
     ushort_int a = 0;
     cout << "Введите сторону номер один: ";
     cin >> a;
